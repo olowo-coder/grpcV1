0cfeb99d0f2dc764b2d511730539efe0f92392b2
olowo-coder <olowofela.olanrewaju@decagon.dev>
2022-04-21 21:26:32 +0100

    Set up

---
+++ src/test/java/com/example/springgrpcv1/SpringGrpcV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springgrpcv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGrpcV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
